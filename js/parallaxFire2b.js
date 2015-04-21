4560db92e2821ee3e25c9a29ede2c0916f72f362
rllhenson <rllhenson@gmail.com>
2015-04-20 22:59:32 -0400

    fire 2b

---
+++ js/parallaxFire2b.js
@@ -0,0 +1,14 @@
+// parallax
+$('#myParallax1').parallax({
+	width: 332,  
+	height: 233,
+	enableMouse: true,
+	activateOnClick: false,
+	sensitivityX: 1,
+	sensitivityY: 1,
+	easingCoefficient: 7,
+	autoZCoordinate: true,
+	useCustomZ: true,
+	focusZ: 100
+}); 
+
