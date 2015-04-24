adf575a1350d53d48598d38734f2ff9495f1a346
rllhenson <rllhenson@gmail.com>
2015-04-23 23:53:55 -0400

    finished scream_2b

--- js/scream2b.js
+++ js/scream2b.js
@@ -1,7 +1,7 @@
 // parallax
 $('#myParallax1').parallax({
-	width: 332,  
-	height: 233,
+	width: 673,  
+	height: 296,
 	enableMouse: true,
 	activateOnClick: false,
 	sensitivityX: 1,
@@ -13,36 +13,8 @@ $('#myParallax1').parallax({
 }); 
 
 $('#myParallax2').parallax({
-	width: 400,  
-	height: 341,
-	enableMouse: true,
-	activateOnClick: false,
-	sensitivityX: 0,
-	sensitivityY: 1,
-	easingCoefficient: 7,
-	autoZCoordinate: true,
-	useCustomZ: true,
-	focusZ: 100
-}); 
-
-$('#myParallax2').hover(function(){
-	$('#catman img').css("opacity","1");
-	$('#text3').css("opacity","1");
-});
-
-$('#allthethings').hover(function(){
-	$('#poof').css("width","200px");
-	$('#poof').css("opacity","0");
-	$('#poof').css("left","1400px");
-	$('#bradcircle').css("opacity","0");
-	$('.big').css("opacity","0.8");
-	$('#catcircle').css("opacity","0");
-	$('#newthings').css("opacity","1");
-});
-
-$('#myParallax3').parallax({
-	width: 372,  
-	height: 219,
+	width: 386,  
+	height: 278,
 	enableMouse: true,
 	activateOnClick: false,
 	sensitivityX: 1,
@@ -52,6 +24,50 @@ $('#myParallax3').parallax({
 	useCustomZ: true,
 	focusZ: 100
 }); 
+
+$('#panel2').hover(function(){
+	$('#text3').css("opacity","1");
+	$('#panel2 img').css("-webkit-transform","rotate(-10deg)");
+	$('#panel2 img').css("transform","rotate(-10deg)");
+	$('#moveit img').css("left","350px");
+});
+
+$('#myParallax3').parallax({
+	width: 484,  
+	height: 314,
+	enableMouse: true,
+	activateOnClick: false,
+	sensitivityX: 1,
+	sensitivityY: 1,
+	easingCoefficient: 7,
+	autoZCoordinate: true,
+	useCustomZ: true,
+	focusZ: 100
+}); 
+
+$('#myParallax3').hover(function(){
+	$('#heart').css("opacity","0");
+	$('#brokenheart').css("opacity","1");
+	$('#tears').css("opacity","1");
+});
+
+$('#myParallax4').parallax({
+	width: 587,  
+	height: 256,
+	enableMouse: true,
+	activateOnClick: false,
+	sensitivityX: 1,
+	sensitivityY: 0,
+	easingCoefficient: 7,
+	autoZCoordinate: true,
+	useCustomZ: true,
+	focusZ: 100
+}); 
+
+$('#myParallax4').hover(function(){
+	$('#flyingspeech').css("opacity","1");
+	$('#flying').css("opacity","1");
+});
 
 var glow = $('.end');
 setInterval(function(){
